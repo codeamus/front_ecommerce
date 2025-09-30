4bfa55b46280de7cdd83c9aba8754cb30062c90e
Alexander Urrutia <matias04041994@gmail.com>
2025-09-30 17:13:21 -0300

    add variables de entorno

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,8 +4,8 @@ import { structureTool } from "sanity/structure";
 import { visionTool } from "@sanity/vision";
 
 export default defineConfig({
-  projectId: "8v549uft",
-  dataset: "production",
+  projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
+  dataset: import.meta.env.PUBLIC_SANITY_DATASET,
   title: "Studio",
   basePath: "/studio",
   plugins: [structureTool(), visionTool()],
